74ec5ff03e7ee4284cda20a34aec89d7d8b699fb
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-11-17 19:42:53 -0700

    Created printDigit method and updated readme file

---
+++ module3.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Jonathan <jonathanmirabile@mail.weber.edu>
+#
+# Distributed under terms of the MIT license.
+import sys
+
+"""
+This program takes user a zip code as input and prints it in bar code form
+    Args:
+        Zipcode
+    Returns:
+        Barcode representation of zipcode
+        : denotes half bars
+        | denotes full bars
+"""
+
+
+def printDigit(d):
+    """
+    Converts a single digit into a barcode representation
+        Args:
+            d = digit
+        Returns:
+            Barcode representation of digit
+            : denotes half bars
+            | denotes full bars
+    """
+    value = {
+            1: ":::||",
+            2: "::|:|",
+            3: "::||:",
+            4: ":|::|",
+            5: ":|:|:",
+            6: ":||::",
+            7: "|:::|",
+            8: "|::|:",
+            9: "|:|::",
+            0: "||:::",
+        }
+    return value.get(d, "Invalid Input")
+
+def printBarCode(zipCode):
+    """
+    Converts a zipcode into barcode format
+    Splits the zipcode and loops through it using printDigit() to convert the number
+        Args:
+            zipCode
+        Returns:
+            Prints zipCode in Barcode format
+            Note: Adds | to beginning and ending as well as Check Digit
+    """
+    
+
+
+
+# Main function
+def main():
+    return
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
+

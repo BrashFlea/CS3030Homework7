a5a8353316315fc988011eb915cb7e2d1d594019
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-11-18 23:18:14 -0700

    Added documentation before submisson

--- module4.py
+++ module4.py
@@ -10,16 +10,29 @@ import urllib
 import urllib.request
 from module3 import printBarCode, printDigit
 """
-
+Test module. 
+Imports module 3 and validates it using data found in external file.
+    Args:
+        none
+    Return:
+        Prints zipcodes used to test and corresponding postal barcodes
 """
 
 def test_zipcodes():
+    """
+    Fetches file used to test module 3 and validates it.
+        Args:
+            none
+        Return:
+            Prints zipcodes used to test module 3 and corresponding postal barcodes
+    """
     url = "http://icarus.cs.weber.edu/~hvalle/cs3030/data/barCodeData.txt"
     resource = urllib.request.urlopen(url)
     content = resource.read().decode().split()
     print("Converting Zipcodes:", content)
     for zipcode in content:
         printBarCode(zipcode)
+
 
 # Main function
 def main():
